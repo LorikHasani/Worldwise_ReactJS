1d0c194defa328db1aa96b44512421b4e8e9c176
LorikHasani <kikpubgm@gmail.com>
2023-07-05 16:03:20 +0200

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import Product from "./pages/Product";
 import Price from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
-
-
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Price />} />
-        <Route path="*" element={<PageNotFound  />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
